ddcce679ae1c1c63a86da96caf3f51e59b38d9c3
marotella <michelle.a.rotella@gmail.com>
2023-03-11 15:37:56 -0500

    updates

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const methodOverride = require("method-override");
 
 // DATABASE CONNECTION
 // How to connect to the database either via heroku or locally
-
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.set("strictQuery", true) //required for it to deploy
+mongoose.connect(MONGODB_URI,{
+    useNewURLParser: false,
+})
 
 
 // Mongo error/success
